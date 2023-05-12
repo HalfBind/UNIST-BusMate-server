9818328c0b49ed36e7217756e69dfc3d8e361635
Gibeom Lim <delphox60@gmail.com>
2023-05-12 19:17:26 +0900

    feat: implement time utility

--- src/main/java/halfbind/UNISTBusMate/domain/Destination.java
+++ src/main/java/halfbind/UNISTBusMate/domain/Destination.java
@@ -1,5 +1,35 @@
 package halfbind.UNISTBusMate.domain;
 
+import java.util.Objects;
+
 public enum Destination {
-    KTX_ULSAN_STATION, SINBOK_ROTARY, EONYANG_TERMINAL, ULSAN_TERMINAL, ULSAN_UNIVERSITY, GUYEONG_RI, SEONGNAM, SAMSAN
-}
+    KTX_ULSAN_STATION, SINBOK_ROTARY, EONYANG_TERMINAL, ULSAN_TERMINAL, ULSAN_UNIVERSITY, GUYEONG_RI, SEONGNAM, SAMSAN;
+
+    public static Destination get(String destinationName) {
+        if (Objects.equals(destinationName, "KTX_ULSAN_STATION")) {
+            return Destination.KTX_ULSAN_STATION;
+        }
+        if (Objects.equals(destinationName, "SINBOK_ROTARY")) {
+            return Destination.SINBOK_ROTARY;
+        }
+        if (Objects.equals(destinationName, "EONYANG_TERMINAL")) {
+            return Destination.EONYANG_TERMINAL;
+        }
+        if (Objects.equals(destinationName, "ULSAN_TERMINAL")) {
+            return Destination.ULSAN_TERMINAL;
+        }
+        if (Objects.equals(destinationName, "ULSAN_UNIVERSITY")) {
+            return Destination.ULSAN_UNIVERSITY;
+        }
+        if (Objects.equals(destinationName, "GUYEONG_RI")) {
+            return Destination.GUYEONG_RI;
+        }
+        if (Objects.equals(destinationName, "SEONGNAM")) {
+            return Destination.SEONGNAM;
+        }
+        if (Objects.equals(destinationName, "SAMSAN")) {
+            return Destination.SAMSAN;
+        }
+        return null;
+    }
+    }
---
+++ src/main/java/halfbind/UNISTBusMate/util/TimeManager.java
@@ -0,0 +1,32 @@
+package halfbind.UNISTBusMate.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TimeManager {
+    public static String addMinutes(String targetTime, Long minutes) {
+        Pattern pattern = Pattern.compile("^([01]?[0-9]|2[0-3]):([0-5][0-9])$");
+        Matcher matcher = pattern.matcher(targetTime);
+        Long targetMinute;
+        Long targetHour;
+        if (matcher.matches()) {
+            targetHour = Long.parseLong(matcher.group(1));
+            targetMinute = Long.parseLong(matcher.group(2));
+        } else {
+            return null;
+        }
+        targetMinute += minutes;
+        if (targetMinute >= 60) {
+            targetMinute -= 60;
+            targetHour += 1;
+        }
+        String result = "";
+        if (targetHour < 10) {
+            result += "0";
+        }
+        result += targetHour.toString();
+        result += ":";
+        result += targetMinute.toString();
+        return result;
+    }
+}
