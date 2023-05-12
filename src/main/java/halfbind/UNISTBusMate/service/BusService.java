03faadd1f7366c9f83c6ad6ba4e740db943020d5
Gibeom Lim <delphox60@gmail.com>
2023-05-12 19:17:42 +0900

    feat: implement create bus

--- src/main/java/halfbind/UNISTBusMate/service/BusService.java
+++ src/main/java/halfbind/UNISTBusMate/service/BusService.java
@@ -11,8 +11,14 @@ public class BusService {
 
     @Autowired
     private BusRepository busRepository;
+    private Long busIdCounter = 0L;
 
     public Bus getBusById(Long id) {
         return busRepository.findById(id).orElse(null);
+    }
+
+    public Bus createBus(Bus bus) {
+        bus.setId(busIdCounter++);
+        return busRepository.save(bus);
     }
 }
