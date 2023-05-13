22786905968b77234ad974d0b7be4f039e4b5a8b
Gibeom Lim <delphox60@gmail.com>
2023-05-13 14:24:09 +0900

    feat: implement comparing time

--- src/main/java/halfbind/UNISTBusMate/util/TimeManager.java
+++ src/main/java/halfbind/UNISTBusMate/util/TimeManager.java
@@ -7,6 +7,7 @@ import org.springframework.data.util.Pair;
 
 public class TimeManager {
     private static final Pattern TIME_PATTERN = Pattern.compile("^([01]?[0-9]|2[0-3]):([0-5][0-9])$");
+    private static final Long HOUR_IN_MINUTES = 60L;
 
     private static Pair<Long, Long> convertStringToLong(String time) {
         Matcher matcher = TIME_PATTERN.matcher(time);
@@ -47,5 +48,16 @@ public class TimeManager {
         Long targetMinute = targetTimeInLong.getSecond();
 
         return convertLongToString(targetHour, targetMinute);
+    }
+
+    public static Boolean isAfter(String targetTime, String standardTime) {
+        Pair<Long, Long> targetTimeInLong = convertStringToLong(targetTime);
+        Long targetHour = targetTimeInLong.getFirst();
+        Long targetMinute = targetTimeInLong.getSecond();
+        Pair<Long, Long> standardTimeInLong = convertStringToLong(standardTime);
+        Long standardHour = standardTimeInLong.getFirst();
+        Long standardMinute = standardTimeInLong.getSecond();
+
+        return HOUR_IN_MINUTES * targetHour + targetMinute >= HOUR_IN_MINUTES * standardHour + standardMinute;
     }
 }
