2db8d4b002ddf1984f7fa26dfb11d62ffbeb558f
Gibeom Lim <delphox60@gmail.com>
2023-05-15 15:32:13 +0900

    refactor: replace @Data to getter/setter implementation

--- src/main/java/halfbind/UNISTBusMate/controller/BookmarkRequestDto.java
+++ src/main/java/halfbind/UNISTBusMate/controller/BookmarkRequestDto.java
@@ -5,9 +5,32 @@ import java.util.List;
 import halfbind.UNISTBusMate.domain.Day;
 import lombok.Data;
 
-@Data
 public class BookmarkRequestDto {
     private Long busId;
     private String userName;
     private List<Day> days;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Long getBusId() {
+        return busId;
+    }
+
+    public List<Day> getDays() {
+        return days;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setBusId(Long busId) {
+        this.busId = busId;
+    }
+
+    public void setDays(List<Day> days) {
+        this.days = days;
+    }
 }
