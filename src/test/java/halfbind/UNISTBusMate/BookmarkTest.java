c2c1078ea4fe4d403bee43c491301e03be8b5916
Gibeom Lim <delphox60@gmail.com>
2023-05-15 14:09:25 +0900

    test: add Bookmark domain constructor test

--- src/test/java/halfbind/UNISTBusMate/BookmarkTest.java
+++ src/test/java/halfbind/UNISTBusMate/BookmarkTest.java
@@ -1,32 +1,33 @@
-// package halfbind.UNISTBusMate;
-//
-// import org.junit.Test;
-//
-// import java.util.Arrays;
-// import java.util.List;
-//
-// import static org.junit.Assert.assertEquals;
-//
-// import halfbind.UNISTBusMate.domain.Bookmark;
-// import halfbind.UNISTBusMate.domain.Destination;
-// import halfbind.UNISTBusMate.domain.Day;
-//
-// public class BookmarkTest {
-//
-//     @Test
-//     public void testBookmark() {
-//         Bookmark bookmark = new Bookmark();
-//
-//         bookmark.setDestination(Destination.KTX_ULSAN_STATION);
-//         bookmark.setArrivalTime("10:30");
-//         bookmark.setUserName("John Doe");
-//         Object Day;
-//         bookmark.setDays(Arrays.asList(halfbind.UNISTBusMate.domain.Day.MON));
-//
-//         assertEquals(bookmark.getDestination(), Destination.KTX_ULSAN_STATION);
-//         assertEquals(bookmark.getArrivalTime(), "10:30");
-//         assertEquals(bookmark.getUserName(), "John Doe");
-//         assertEquals(bookmark.getDays(), Arrays.asList(halfbind.UNISTBusMate.domain.Day.MON));
-//     }
-// }
-//
+package halfbind.UNISTBusMate;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+import halfbind.UNISTBusMate.domain.Bookmark;
+import halfbind.UNISTBusMate.domain.Bus;
+import halfbind.UNISTBusMate.domain.Destination;
+import halfbind.UNISTBusMate.domain.Day;
+
+public class BookmarkTest {
+
+    @Test
+    public void testBookmark() {
+        Bookmark bookmark = new Bookmark();
+
+        Bus bus = new Bus();
+        bus.setId(1L);
+        bookmark.setBus(bus);
+        bookmark.setUserName("Zelda");
+        Object Day;
+        bookmark.setDays(Arrays.asList(new Day[] {halfbind.UNISTBusMate.domain.Day.MON}));
+
+        assertEquals(bookmark.getBus(), bus);
+        assertEquals(bookmark.getUserName(), "Zelda");
+        assertEquals(bookmark.getDays(), Arrays.asList(halfbind.UNISTBusMate.domain.Day.MON));
+    }
+}
+
