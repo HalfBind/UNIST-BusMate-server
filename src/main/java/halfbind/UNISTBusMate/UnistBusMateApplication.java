3c8e706b44222d17f54f3efa44b8dc86f5ad9ece
JinsuKim <coldeargrey@gmail.com>
2023-06-18 08:32:50 +0900

    Build test (#9)
    * test-1
    
    * cors & resource return issue fixed

--- src/main/java/halfbind/UNISTBusMate/UnistBusMateApplication.java
+++ src/main/java/halfbind/UNISTBusMate/UnistBusMateApplication.java
@@ -3,6 +3,10 @@ package halfbind.UNISTBusMate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class UnistBusMateApplication extends SpringBootServletInitializer  {
@@ -10,5 +14,15 @@ public class UnistBusMateApplication extends SpringBootServletInitializer  {
 	public static void main(String[] args) {
 		SpringApplication.run(UnistBusMateApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 
 }
