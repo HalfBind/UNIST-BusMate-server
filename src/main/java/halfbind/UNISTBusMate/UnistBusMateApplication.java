aba6b9e43784eee867213211e78af6ad63bd3acd
KimJinsu <sis3141@unist.ac.kr>
2023-06-18 10:13:36 +0000

    Critical fixes cors handling for dev - post, delete method direct url routing handle

--- src/main/java/halfbind/UNISTBusMate/UnistBusMateApplication.java
+++ src/main/java/halfbind/UNISTBusMate/UnistBusMateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class UnistBusMateApplication extends SpringBootServletInitializer  {
@@ -20,7 +21,14 @@ public class UnistBusMateApplication extends SpringBootServletInitializer  {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+								.allowedMethods(
+    							HttpMethod.GET.name(),
+									HttpMethod.HEAD.name(),
+									HttpMethod.POST.name(),
+									HttpMethod.PUT.name(),
+									HttpMethod.DELETE.name()
+								);
             }
         };
     }
